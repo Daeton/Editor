03454d0bfc1efe24d297c96e77a27205e4813d8e
IHateYourCode <89636625+IHateYourCode@users.noreply.github.com>
2022-06-23 05:05:34 -0400

    Added Linux paths

---
+++ Source/System/Files/Paths.js
@@ -0,0 +1,28 @@
+
+import { join } from 'Path';
+
+const { env } = Deno;
+
+
+/*
+ *  User Data Folder
+ */
+
+export const home = env.get('HOME');
+
+export const data = join(home,'.Daeton');
+
+export const packages = join(data,'Packages');
+
+export const configs = join(data,'Configurations');
+
+
+/*
+ *  Application Data Folder
+ */
+
+export const app = join('usr','share','Daeton');
+
+export const info = join(app,'Info');
+
+export const version = join(info,'Version');
